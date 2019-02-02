7e9482c30e3903137529ae7990debe0661485cb4
Day Souza <daysouza@DayMac.local>
2019-02-02 00:51:28 -0200

    Added new categories

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -5,9 +5,10 @@ export class InMemoryDatabase implements InMemoryDbService {
   createDb() {
     const categories: Category[] = [
       {id: 1, name: 'Leisure', description: 'Movies, parks, beach...'},
-      {id: 2, name: 'Health', description: 'Movies, parks, beach...'},
+      {id: 2, name: 'Health', description: 'Medicines, hospital...'},
       {id: 3, name: 'Books', description: 'Books, e-books'},
-      {id: 4, name: 'Salary', description: 'Company salary'}
+      {id: 4, name: 'Salary', description: 'Paycheck'},
+      {id: 5, name: 'Savings', description: 'Savings'},
     ];
 
     return { categories };
