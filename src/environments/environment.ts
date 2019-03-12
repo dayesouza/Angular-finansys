8047976ef2557a10f0bf682b4861453002bcc3ad
Day Souza <day.carol18@gmail.com>
2019-03-12 01:41:21 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyDtkls39fgyQ7wjc8MDnQJnWel-7tRmv1A',
-    authDomain: 'green-wallet-c234f.firebaseapp.com',
-    databaseURL: 'https://green-wallet-c234f.firebaseio.com',
-    projectId: 'green-wallet-c234f',
-    storageBucket: 'green-wallet-c234f.appspot.com',
-    messagingSenderId: '337246973894'
-
-  }
+  
 };
